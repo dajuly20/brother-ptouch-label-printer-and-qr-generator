2dae24d71d5b8a87445d508e19ec98ed30d9ea8f
Julian Wiche <julian@wiche.eu>
2024-03-24 19:44:17 +0100

    added more necessary installs beforehand

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,23 +1,26 @@
-
 #!/bin/bash
-
-PACKAGES="build-essential cmake gettext libgd-dev libusb-dev libusb-1.0-0-dev git"
 
 sudo apt update
 sudo apt upgrade
 echo "Installing Packages: $PACKAGES"
-sudo apt install "$PACKAGES"
-
+sudo apt install build-essential cmake gettext libgd-dev libusb-dev libusb-1.0-0-dev git autoconf automake cmake autotools-dev
 cd ~ 
-git clone https://github.com/philpem/printer-driver-ptouch 
-cd -
-echo ""
-echo "**************************************************************
-echo "*                                                            *
-echo "*    https://github.com/philpem/printer-driver-ptouch        *
-echo "*    PLEAS FIRST FOLLOW THE INSTRUCTIONS HERE"               *
-echo "*    -->  ~/printer-driver-ptouch                            *
-echo "*    (at least I already cloned it for you ;-))              *
-echo "*                                                            *
-echo "*************************************************************"
+url="https://github.com/philpem/printer-driver-ptouch"
+folder="~/printer-driver-ptouch"
+if ! git clone "${url}" "${folder}" 2>/dev/null && [ -d "${folder}" ] ; then
+     git pull "${folder}" 
+     echo "Repo was there already. Pulled to update."
+fi
+
+
+echo ''
+echo ' **************************************************************'
+echo ' *                                                            *'
+echo " *    "$url"        *"
+echo ' *    PLEAS FIRST FOLLOW THE INSTRUCTIONS HERE                *'
+echo ' *    -->  ~/printer-driver-ptouch                            *'
+echo ' *    (at least I already cloned it for you ;-))              *'
+echo ' *                                                            *'
+echo ' *************************************************************'
 echo "So I couldn't find reason in automating this step. " 
+
